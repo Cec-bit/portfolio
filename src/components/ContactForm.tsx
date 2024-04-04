0265ce34b1d74fc1b3fc35b313c9fec65bd70ccd
cec-bit <cecgod1400@gmail.com>
2024-04-04 09:24:29 -0400

    Integrated css sheets into globals.css

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import "./ContactForm.css";
 import axios from 'axios'; // Import axios for making HTTP requests
 
 function ContactForm() {
--- src/components/Hover.tsx
+++ src/components/Hover.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import "./Hover.css";
 
 interface Option {
   title: string;
